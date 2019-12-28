e14a3cfb09384b9145143bbb15f0118f4a41f11d
TRHX <tanrenhou@126.com>
2019-12-28 23:29:56 +0800

    第三十七次提交-修正js错误

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,1 +1,21 @@
-var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.0/images/title-icon1.png");document.title="快回来~页面崩溃了！";clearTimeout(titleTime)}else{$("[rel='icon']").attr("href","https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.0/images/title-icon2.png");document.title="其实并没有~";titleTime=setTimeout(function(){document.title=OriginTitle;$("[rel='icon']").attr("href","https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.0/images/favicon-32x32.png")},2000)}});
+/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
+window.onload = function() {
+    var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function() {
+      if(document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/failure.ico");
+        $('[rel="shortcut icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/failure.ico");
+        document.title = '喔唷，崩溃啦！';
+        clearTimeout(titleTime);
+      } else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/favicon.ico");
+        $('[rel="shortcut icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/favicon.ico");
+        document.title = '咦，页面又好了！';
+        titleTime = setTimeout(function() {
+          document.title = OriginTitile;
+        }, 2000);
+      }
+    });
+  }
+  
