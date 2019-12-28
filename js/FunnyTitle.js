2e7593663e4b1e1d2634ba78f93dba41ebf08bd6
TRHX <tanrenhou@126.com>
2019-12-29 00:03:11 +0800

    第三十八次提交-修正js错误

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,21 +1,17 @@
-/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
-window.onload = function() {
-    var OriginTitile = document.title;
-    var titleTime;
-    document.addEventListener('visibilitychange', function() {
-      if(document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/failure.ico");
-        $('[rel="shortcut icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/failure.ico");
-        document.title = '喔唷，崩溃啦！';
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.2/images/failure.ico");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
-      } else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/favicon.ico");
-        $('[rel="shortcut icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.1/images/favicon.ico");
-        document.title = '咦，页面又好了！';
-        titleTime = setTimeout(function() {
-          document.title = OriginTitile;
+    }
+    else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.2/images/failure.ico");
+        document.title = '(ฅ>ω<*ฅ) 噫又好了 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
         }, 2000);
-      }
-    });
-  }
-  
+    }
+});
